f82a93d304c70c2280484d4f5e679197fd4012d9
LuthfiyyahA <luthfiyyahadzka@gmail.com>
2024-11-21 11:23:16 +0700

    login

--- app/view/login.php
+++ app/view/login.php
@@ -4,20 +4,50 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login SIBETA!</title>
+    <link rel="stylesheet" href=".../css/login.css">
 </head>
 
 <body>
-    <h1>Log In</h1>
-    <form action="../controller/login.php" method="POST">
-        <input type="radio" name="role" value="admin" id="radioAdmin">
-        <label for="role">Admin</label>
-        <input type="radio" name="role" value="mahasiswa" id="radioMhs">
-        <label for="role">Mahasiswa</label><br><br>
-        <input type="text" name="username" required" id="username">
-        <input type="password" name="password" placeholder="Password" required>
-        <input type="submit" value="Login">
-    </form>
+    <div class="container">
+            <div class="login-box">
+                <h1 class="welcome">Welcome to <span class="blue-text">SIBETA!</span></h1>
+                <br><br>
+                
+                <form class="login-form" action="../controller/login.php" method="POST">
+                    <div class="">
+                        <input type="radio" name="role" value="admin" id="radioAdmin">
+                        <label for="role">Admin</label>
+                        <input type="radio" name="role" value="mahasiswa" id="radioMhs">
+                        <label for="role">Mahasiswa</label><br><br>
+                    </div>
+                    <div class="form-group">
+                        <label for="nim">NIM atau Email</label>
+                        <div class="input-container">
+                            <input type="text" id="nim" name="nim">
+                            <span class="user-icon"></span>
+                        </div>
+                    </div>
+                    
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <div class="input-container">
+                            <input type="password" id="password" name="password">
+                            <span class="eye-icon"></span>
+                        </div>
+                    </div>
+                    
+                    <div class="form-options">
+                        <div class="remember-me">
+                            <input type="checkbox" id="remember" name="remember">
+                            <label for="remember">Remember me</label>
+                        </div>
+                    </div>
+                    
+                    <button type="submit" class="login-button">LOGIN</button>
+                </form>
+            </div>
+        </div>
 
     <script src="../../js/login.js"></script>
 </body>
