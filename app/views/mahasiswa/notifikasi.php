fc937d4766cd26aaef7a7453276c2db60e1d0519
LuthfiyyahA <luthfiyyahadzka@gmail.com>
2024-12-12 01:43:26 +0700

    penambahan file notifikasi

---
+++ app/views/mahasiswa/notifikasi.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="/../SI_BebasTanggungan_TA/image/icon.png">
+        <link rel="stylesheet" href="/../SI_BebasTanggungan_TA/css/Mahasiswa/notifikasi.css">
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="top">
+                <h2>NOTIFICATION</h2>
+            </div>
+
+            <div class="content-container">
+                <div class="today">
+                    <div class="content">
+                        <img src="/../SI_BebasTanggungan_TA/image/pp.png" alt="">
+                        <div class="isi">
+                            <span>Admin commented on your Thesis Report</span>
+                            <span>3 hours ago</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="yesterday">
+                    <div class="content">
+                        <img src="/../SI_BebasTanggungan_TA/image/pp.png" alt="">
+                        <div class="isi">
+                            <span>Teknisi has been verified your 3 files</span>
+                            <span>01.26 PM</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
